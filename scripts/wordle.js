0aca6785378ac80ce4b021fcee1b1626f683a14a
Bert Joseph Prestoza <87596273+bertjosephp@users.noreply.github.com>
2024-05-01 00:29:10 -0700

    Implement Wordle page, styles, and game logic

---
+++ scripts/wordle.js
@@ -0,0 +1,80 @@
+const rows = document.querySelectorAll('.row');
+const correctAnswer = "APPLE";
+let isAnswerCorrect = false;
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    let currentRow = 0;     // first row
+    disableAllInputs();
+    enableRow(rows[currentRow]);
+})
+
+function disableAllInputs() {
+    rows.forEach(row => {
+        const inputs = row.querySelectorAll('input[type="text"]');
+        inputs.forEach(input => {
+            input.disabled = true;
+        })
+    })
+}
+
+function enableRow(row) {
+    const inputs = row.querySelectorAll('input[type="text"]');
+    inputs[0].disabled = false;
+    inputs[0].focus();
+    inputs.forEach(input => {
+        input.addEventListener('input', () => {
+            moveFocusToNextInput(input, inputs);
+        })
+        input.addEventListener('keydown', (event) => {
+            if (event.key === 'Enter') {
+                checkIfRowIsFull(row, inputs);
+            }
+        })
+        
+    })
+}
+
+function moveFocusToNextInput(currentInput, rowInputs) {
+    const currentIndex = Array.from(rowInputs).indexOf(currentInput);
+    if (0 <= currentIndex && currentIndex < rowInputs.length - 1) {
+        const nextInput = rowInputs[currentIndex + 1];
+        nextInput.disabled = false;
+        currentInput.disabled = true;
+        nextInput.focus();
+    }
+}
+
+function checkIfRowIsFull(row, rowInputs) {
+    const isFull = Array.from(rowInputs).every(rowInput => rowInput.value.length === rowInput.maxLength);
+    if (isFull) {
+        verifyAnswer(rowInputs);
+        if (isAnswerCorrect) {
+            disableAllInputs();
+            alert("You win!");
+        }
+        else if (row.nextElementSibling) {
+            enableRow(row.nextElementSibling)
+        } else {
+            disableAllInputs();
+            alert("You lose!\nThe hidden word is: " + correctAnswer);
+        }
+    }
+}
+
+function verifyAnswer(rowInputs) {
+    const inputAnswer = Array.from(rowInputs).map(input => input.value).join('');
+    Array.from(rowInputs).forEach((rowInput, index) => {
+        if (correctAnswer[index] === rowInput.value) {
+            rowInput.classList.add('correct-letter');
+        } else if (correctAnswer.includes(rowInput.value)) {
+            rowInput.classList.add('wrong-position');
+        } else {
+            rowInput.classList.add('wrong-letter');
+        }
+        rowInput.disabled = true;
+    })
+    if (inputAnswer === correctAnswer) {
+        isAnswerCorrect = true;
+    }
+}
