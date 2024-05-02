79c405ffee063e61565591719e4bdffa97d5654c
Bert Joseph Prestoza <87596273+bertjosephp@users.noreply.github.com>
2024-05-02 00:29:26 -0700

    Generate list of words from txt file

---
+++ data/load_words.py
@@ -0,0 +1,20 @@
+def read_words_from_file(filepath):
+    words = []
+    with open(filepath, 'r') as file:
+        for line in file:
+            words.append(line.strip())
+    return words
+
+def write_words_to_js(words, output_filepath):
+    with open(output_filepath, 'w') as file:
+        file.write("const listOfWords = [")
+        for word in words:
+            file.write(f'{word}, ')
+        file.write("];\n\n")
+        file.write("export default listOfWords;")
+
+filepath = './wordle-nyt-answers-alphabetical.txt'
+output_filepath = '../scripts/words.js'
+
+words = read_words_from_file(filepath)
+write_words_to_js(words, output_filepath)
